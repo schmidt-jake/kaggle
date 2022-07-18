7380e51b8601da3847119c84af26ee29e1ae0c75
Jake Schmidt <jake.schmidt@utexas.edu>
2022-07-18 10:33:54 -0600

    improve `bin/lock.sh`

--- bin/lock.sh
+++ bin/lock.sh
@@ -6,4 +6,9 @@ pip install -U pip setuptools wheel
 
 pip install pip-compile-multi
 
-pip-compile-multi --autoresolve --live --upgrade --directory requirements
+pip-compile-multi \
+    --autoresolve \
+    --live \
+    --upgrade \
+    --use-cache \
+    --directory=requirements
