46eb9c586b7d5928f34694163b8fb26b720a5750
Jake Schmidt <jake.schmidt@utexas.edu>
2022-12-19 13:26:17 -0700

    simplify `lock.sh`

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,12 +2,9 @@
 
 set -euo pipefail
 
-UPGRADE_FLAG=${1:-"--no-upgrade"}
-
 pip-compile-multi \
     --autoresolve \
     --live \
-    $UPGRADE_FLAG \
     --use-cache \
     --no-backtracking \
-    --directory=$2
+    $@
