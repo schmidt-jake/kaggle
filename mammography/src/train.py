2d4a24693d169476ee5c17031a63e1693c9832b9
Jake Schmidt <jake.schmidt@utexas.edu>
2023-01-01 22:46:52 -0700

    fix bug

--- mammography/src/train.py
+++ mammography/src/train.py
@@ -314,7 +314,7 @@ class Model(pl.LightningModule):
 
     def training_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
         prediction: torch.Tensor = self(batch["pixels"])
-        wandb.log({"predictions/train": wandb.Histogram(prediction.detach())}, step=self.global_step)
+        wandb.log({"predictions/train": wandb.Histogram(prediction.detach().cpu())}, step=self.global_step)
         loss: torch.Tensor = self.loss(input=prediction, target=batch["cancer"].float())
         self.train_metrics(preds=prediction.sigmoid(), target=batch["cancer"])
         self.log_dict(self.train_metrics, on_step=True, on_epoch=False)  # type: ignore[arg-type]
@@ -322,7 +322,7 @@ class Model(pl.LightningModule):
 
     def validation_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
         prediction: torch.Tensor = self(batch["pixels"])
-        wandb.log({"predictions/val": wandb.Histogram(prediction.detach())}, step=self.global_step)
+        wandb.log({"predictions/val": wandb.Histogram(prediction.detach().cpu())}, step=self.global_step)
         loss: torch.Tensor = self.loss(input=prediction, target=batch["cancer"].float())
         self.val_metrics(preds=prediction.sigmoid(), target=batch["cancer"])
         self.log_dict(self.val_metrics, on_step=False, on_epoch=True)  # type: ignore[arg-type]
