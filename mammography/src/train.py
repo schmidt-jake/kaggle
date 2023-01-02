47a5149e0e2e65e7ba07908b6e5d242caf473475
Jake Schmidt <jake.schmidt@utexas.edu>
2023-01-01 22:03:17 -0700

    fix bug

--- mammography/src/train.py
+++ mammography/src/train.py
@@ -281,16 +281,16 @@ class Model(pl.LightningModule):
         return predictions.squeeze(dim=1)
 
     def training_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
-        prediction = self(batch["pixels"])
-        loss = self.loss(input=prediction, target=batch["cancer"].float())
-        self.train_metrics(preds=prediction, target=batch["cancer"])
+        prediction: torch.Tensor = self(batch["pixels"])
+        loss: torch.Tensor = self.loss(input=prediction, target=batch["cancer"].float())
+        self.train_metrics(preds=prediction.sigmoid(), target=batch["cancer"])
         self.log_dict(self.train_metrics, on_step=True, on_epoch=False)  # type: ignore[arg-type]
         return {"loss": loss}
 
     def validation_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> None:
-        prediction = self(batch["pixels"])
-        loss = self.loss(input=prediction, target=batch["cancer"].float())
-        self.val_metrics(preds=prediction, target=batch["cancer"])
+        prediction: torch.Tensor = self(batch["pixels"])
+        loss: torch.Tensor = self.loss(input=prediction, target=batch["cancer"].float())
+        self.val_metrics(preds=prediction.sigmoid(), target=batch["cancer"])
         self.log_dict(self.val_metrics, on_step=False, on_epoch=True)  # type: ignore[arg-type]
         return {"loss": loss}
 
